335b3e77583bfb661bbd18fe304395d74f32b75b
Kevin Ogden <kevinogdendev@gmail.com>
2019-05-20 16:13:27 -0400

    added unit test

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,29 @@
 pragma solidity ^0.5.0;
 
 contract Election {
+	// Model candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
 	// Store candidate
-	// Read candidate
-	string public candidate;
+
+	// Generate candidates function automatically that will allow us to fetch out candidates
+	mapping(uint => Candidate) public candidates;
+
+	// Store Candidates count
+	uint public candidatesCount;
 
 	// Constructor
-	constructor () public {
-		candidate = "Candidate 1";
+	constructor() public {
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate(string memory _name) private {
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 }
